ee71c8a74b8484eea9c97d56a99c7419acc208fe
Moataz <moatazw72@gmail.com>
2024-02-19 23:45:22 +0200

    a

--- board/app/page.tsx
+++ board/app/page.tsx
@@ -1,8 +1,7 @@
 import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
-   <Button variant="default" size="lg"    >  
-    Click Me
-   </Button>
+    <p> Hello World</p>
+   
   )
 }
